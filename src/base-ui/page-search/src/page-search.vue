009d079b961729ef36cab3223de99c1325a822ae
wyy <807515004@qq.com>
2022-04-30 09:53:33 +0800

    fix(搜索): 组件拆分完善

---
+++ src/base-ui/page-search/src/page-search.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="page-search">
+    <yy-form v-bind="formConfig" v-model="formData">
+      <template #header>
+        <h2>高级搜索</h2>
+      </template>
+      <template #footer>
+        <div style="text-align: right">
+          <el-button type="primary">搜索</el-button>
+          <el-button type="primary">重置</el-button>
+        </div>
+      </template>
+    </yy-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import YyForm from "@/base-ui/form";
+export default defineComponent({
+  components: {
+    YyForm
+  },
+  props: {
+    formConfig: {
+      type: Object,
+      required: true
+    }
+  },
+  setup() {
+    const formData = ref({
+      username: "",
+      password: "",
+      sport: "",
+      createdtime: ""
+    });
+    return {
+      formData
+    };
+  }
+});
+</script>
+
+<style scoped></style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,32 +1,24 @@
 <template>
   <div class="user">
-    <h2>用户管理界面</h2>
     <div class="from">
-      <yy-form v-bind="formConfig" v-model="formData"></yy-form>
+      <page-search :formConfig="formConfig"></page-search>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
-import YyForm from "@/base-ui/form";
+import { defineComponent } from "vue";
+import PageSearch from "@/base-ui/page-search";
 import { formConfig } from "./config/search.config";
 
 export default defineComponent({
   name: "user",
   components: {
-    YyForm
+    PageSearch
   },
   setup() {
-    const formData = ref({
-      username: "",
-      password: "",
-      sport: "",
-      createdtime: ""
-    });
     return {
-      formConfig,
-      formData
+      formConfig
     };
   }
 });
